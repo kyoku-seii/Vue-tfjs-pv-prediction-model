3495e225f21af62716856ff10556d2596e6034ea
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-11-26 15:20:31 +0900

    完成了项目意义的书写

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue-tfjs-pv-prediction-model/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/Vue-tfjs-pv-prediction-model/'
+  //   : '/',
   devServer: { port: 8001 }
 }
